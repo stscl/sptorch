5d324d2059e9a010e517b757d3d4f1f42f7435f9
SpatLyu <lyu.geosocial@gmail.com>
2024-08-19 23:04:56 +0800

    import pipe

---
+++ R/pipes.R
@@ -0,0 +1,11 @@
+#' @title Pipe operator
+#' @description
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @usage lhs \%>\% rhs
+#' @rdname pipe
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @export
+NULL
